d851040fe69a1c0978961295a362a4a36dbf16b4
Ti-tanium <1158976264@qq.com>
2019-01-16 12:39:25 +0800

    initiate

--- app/src/main/java/com/example/asus/express/home/HomeFragment.java
+++ app/src/main/java/com/example/asus/express/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.asus.express.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,10 +8,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.asus.express.R;
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.common.Constant;
+
+import static android.app.Activity.RESULT_OK;
 
 public class HomeFragment extends Fragment {
+    private Button mScanButton;
+    private TextView mResult;
+    public static int REQUEST_CODE_SCAN=1;
+
     public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
         return fragment;
@@ -20,6 +31,29 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_home,container,false);
+        mScanButton=view.findViewById(R.id.home_scan_btn);
+        mResult=view.findViewById(R.id.home_result);
+        mScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CaptureActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_SCAN);
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 扫描二维码/条码回传
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) {
+
+                String content = data.getStringExtra(Constant.CODED_CONTENT);
+                mResult.setText("扫描结果为：" + content);
+            }
+        }
     }
 }
